714a81e0b159d1c0f65bb24c0216362e094ec18f
lekhan7 <ktkarumbaiah@gmail.com>
2026-01-21 12:53:25 +0530

    Lake

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import {  FaEnvelope, FaPhone, FaMapMarkerAlt, } from 'react-icons/fa';
 import './Footer.css';
 
 const Footer = () => {
